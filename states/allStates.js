06f547aa8dee5075954e24bb4e2b08fafd5ee395
texasjkee <gt.gluck@gmail.com>
2023-03-20 19:23:08 +0200

    add: states

---
+++ states/allStates.js
@@ -0,0 +1,51 @@
+Alabama  
+Alaska 
+Arizona
+Arkansas 
+California  
+Colorado  
+Connecticut  
+Delaware  
+Florida  
+Georgia  
+Hawaii  
+Idaho  
+Illinois  
+Indiana  
+Iowa  
+Kansas  
+Kentucky  
+Louisiana  
+Maine  
+Massachusetts  
+Maryland  
+Michigan  
+Minnesota  
+Mississippi  
+Missouri  
+Montana  
+Nevada  
+Nebraska  
+New Hampshire  
+New Jersey  
+New Mexico  
+New York  
+North Carolina  
+North Dakota 
+Oklahoma  
+Ohio  
+Oregon  
+Pennsylvania  
+Rhode Island  
+South Carolina  
+South Dakota  
+Tennessee  
+Texas  
+Utah  
+Vermont  
+Virginia  
+Washington  
+Washington   
+West Virginia  
+Wisconsin  
+Wyoming  
