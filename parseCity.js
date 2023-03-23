7ba101fbf8d9b76f46fed07282f557e5542b698a
texasjkee <gt.gluck@gmail.com>
2023-03-23 17:03:58 +0200

    add: transformJSONtoValidOutPut function

---
+++ parseCity.js
@@ -0,0 +1,16 @@
+const company = require('./cities/abbeville.json');
+
+function transformJSONtoValidOutPut(jsonArg) {
+  const transformToValidArray = jsonArg.addressData.toString().split(/(\n+)/)
+
+  const companyMC = `MC Number: ${jsonArg.sinceData[11]}`
+  const companyName = `${transformToValidArray[2]}`
+  const companyPhone = `${transformToValidArray[12]}`
+  
+  const companySinceNumber = +jsonArg.sinceData[21];   
+  const companySince = companySinceNumber ? companySinceNumber : ' ';
+ 
+  return `${companyMC} ${companyName}${companyPhone} Sience: ${companySince}` 
+}
+
+console.log(transformJSONtoValidOutPut(company))
