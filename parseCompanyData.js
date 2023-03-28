d896fed73d909438aacd6e5be1db4cdd2a1d6461
texasjkee <gt.gluck@gmail.com>
2023-03-28 20:24:09 +0300

    delete: MC number

--- parseCompanyData.js
+++ parseCompanyData.js
@@ -1,11 +1,22 @@
 module.exports = function transformJSONtoValidOutPut(jsonArg) {
+  console.log(jsonArg.sinceData[23])
+
   const transformToValidArray = jsonArg.addressData.toString().split(/(\n+)/)
-  const companyMC = `MC Number: ${jsonArg.sinceData[11]}`
+  const companyMC = `${jsonArg.sinceData[11]}`
   const companyName = `${transformToValidArray[2]}`
   const companyPhone = `${transformToValidArray[12]}`
-  
-  const companySinceNumber = +jsonArg.sinceData[21];   
-  const companySince = companySinceNumber ? companySinceNumber : ' ';
- 
-  return `${companyMC} ${companyName}${companyPhone} Sience: ${companySince}` 
+  const city = jsonArg.city;
+  const state = jsonArg.state;
+
+  const companySinceNumber = +jsonArg.sinceData[23];   
+  let companySince = '';
+
+  if (companySinceNumber) {
+    if(companySinceNumber.toString().length === 8) {
+      companySince = companySinceNumber;
+    }
+  } else { companySince = ' '; } 
+
+  // companySinceNumber ? companySinceNumber : ' ';
+  return `"${state}", "${city}", "${companyMC}", "${companyName}", "${companyPhone}", "Sience: ${companySince}" \n` 
 }
