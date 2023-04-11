8e01a34196b34ca049bb2650f022279f8463904e
texasjkee <gt.gluck@gmail.com>
2023-04-11 12:03:21 +0300

    fix: finalFunc.js

--- finalFunc.js
+++ finalFunc.js
@@ -1,4 +1,4 @@
 module.exports = transformAllDataToStr = data => {
   const mcNumber = data.mc !== null ? data.mc.match(/(\d+)/g)[0] : null;
-  return `${data.state}, ${data.city}, ${mcNumber}, ${data.name}, ${data.phone}, ${data.since} \n`
+  return `${data.state}/ ${data.city}/ ${mcNumber}/ ${data.name}/ ${data.phone}/ ${data.since} \n`
 }
