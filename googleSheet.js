554e3705fd2ec540eeff39b272459c3f0a3372d0
texasjkee <gt.gluck@gmail.com>
2023-04-11 12:02:33 +0300

    fix: validGoogleRows

--- googleSheet.js
+++ googleSheet.js
@@ -1,41 +1,35 @@
-const { GoogleSpreadsheet } = require('google-spreadsheet');
-const fs = require('fs');
+const CURRENT_CITY = 'auburn';
 
-const RESPONSES_SHEET_ID = '1J2MoR48cYnPVN9X9rY3l32zp_T9tml63VpEUeTIZwTk';
+const fs = require('fs');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+
 const CREDENTIALS = JSON.parse(fs.readFileSync('credention.json'));
+const RESPONSES_SHEET_ID = '1J2MoR48cYnPVN9X9rY3l32zp_T9tml63VpEUeTIZwTk';
 const doc = new GoogleSpreadsheet(RESPONSES_SHEET_ID);
 
-const getRow = async (email) => {
+const strCities = fs.readFileSync(`./states/alabama/${CURRENT_CITY}.txt', 'utf-8`);
+const arrCities = strCities.split('\n');
 
-    // use service account creds
-    await doc.useServiceAccountAuth({
-        client_email: CREDENTIALS.client_email,
-        private_key: CREDENTIALS.private_key
-    });
+const validGoogleRows = [];
 
-    // load the documents info
-    await doc.loadInfo();
-
-    // Index of the sheet
-    let sheet = doc.sheetsByIndex[1];
-
-    // Get all the rows
-    let rows = await sheet.getRows();
-    console.log(rows[0]._rawData);
-    for (let index = 0; index < rows.length; index++) {
-        const row = rows[index];
-        if (row.email == email) {
-            console.log(row.user_name);
-            console.log(row.password);
+arrCities.forEach(city => {
+    const allCityData = city.split('/').filter(data => data = data.length > 3)
+        const validObj = {
+            state: allCityData[0],
+            city: allCityData[1],
+            mc_number: allCityData[2],
+            company: allCityData[3],
+            phone: allCityData[4],
+            since: allCityData[5],
         }
-    };
-};
-
-getRow('email@gmail.com');
+    const valid = validObj.phone !== undefined;
+    if(valid) {
+        validGoogleRows.push(validObj);
+    }   
+})
 
 const addRow = async (rows) => {
 
-    // use service account creds
     await doc.useServiceAccountAuth({
         client_email: CREDENTIALS.client_email,
         private_key: CREDENTIALS.private_key
@@ -52,67 +46,4 @@ const addRow = async (rows) => {
     }
 };
 
-let rows = [{
-    email: 'email@email.com',
-    user_name: 'ramesh',
-    password: 'abcd@1234'
-}, {
-    email: 'email@gmail.com',
-    user_name: 'dilip',
-    password: 'abcd@1234'
-}];
-
-// addRow(rows);
-
-const updateRow = async (keyValue, oldValue, newValue) => {
-
-    // use service account creds
-    await doc.useServiceAccountAuth({
-        client_email: CREDENTIALS.client_email,
-        private_key: CREDENTIALS.private_key
-    });
-
-    await doc.loadInfo();
-
-    // Index of the sheet
-    let sheet = doc.sheetsByIndex[0];
-
-    let rows = await sheet.getRows();
-
-    for (let index = 0; index < rows.length; index++) {
-        const row = rows[index];
-        if (row[keyValue] === oldValue) {
-            rows[index][keyValue] = newValue;
-            await rows[index].save();
-            break; 
-        }
-    };
-};
-
-// updateRow('email', 'email@gmail.com', 'ramesh@ramesh.com')
-
-const deleteRow = async (keyValue, thisValue) => {
-
-    // use service account creds
-    await doc.useServiceAccountAuth({
-        client_email: CREDENTIALS.client_email,
-        private_key: CREDENTIALS.private_key
-    });
-
-    await doc.loadInfo();
-
-    // Index of the sheet
-    let sheet = doc.sheetsByIndex[0];
-
-    let rows = await sheet.getRows();
-
-    for (let index = 0; index < rows.length; index++) {
-        const row = rows[index];
-        if (row[keyValue] === thisValue) {
-            await rows[index].delete();
-            break; 
-        }
-    };
-};
-
-// deleteRow('email', 'ramesh@ramesh.com');
+addRow(validGoogleRows);
